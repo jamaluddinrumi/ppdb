068415778ba4ef29268c7c5e4258ce4174932cf0
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-07-10 22:45:52 +0700

    autoload grocery_CRUD

--- admin/application/config/autoload.php
+++ admin/application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'ion_auth', 'upload');
+$autoload['libraries'] = array('database', 'ion_auth', 'upload', 'grocery_CRUD');
 
 
 /*
