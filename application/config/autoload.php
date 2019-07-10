4dc9f12a5c404e4b9a21799fb5890770fdbad7f9
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-07-10 17:19:17 +0700

    autoload form helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -90,7 +90,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'inflector');
+$autoload['helper'] = array('url', 'inflector', 'form');
 
 /*
 | -------------------------------------------------------------------
