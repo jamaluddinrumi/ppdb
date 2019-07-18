bd4c094e241c8814d8b086ab53e99f4c012a99b4
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-07-19 00:48:40 +0700

    update sql file

--- u4354245_igni274.sql
+++ u4354245_igni274.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 18, 2019 at 06:00 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.4
+-- Generation Time: Jul 18, 2019 at 05:47 PM
+-- Server version: 10.2.25-MariaDB
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `u4354245_igni274`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `berita`
+--
+
+CREATE TABLE `berita` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(128) NOT NULL,
+  `berita` text NOT NULL,
+  `tanggal_dipublikasikan` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `berita`
+--
+
+INSERT INTO `berita` (`id`, `judul`, `berita`, `tanggal_dipublikasikan`) VALUES
+(1, 'Nullam id dolor id nibh ultricies vehicula ut id elit.', 'Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula porta felis euismod semper.\r\n\r\nAenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus varius blandit sit amet non magna.\r\n\r\nVestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.\r\n\r\nMaecenas faucibus mollis interdum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui.', '2019-06-12');
 
 -- --------------------------------------------------------
 
@@ -54,6 +74,25 @@ CREATE TABLE `login_attempts` (
   `login` varchar(100) NOT NULL,
   `time` int(11) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaturan`
+--
+
+CREATE TABLE `pengaturan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `isi` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pengaturan`
+--
+
+INSERT INTO `pengaturan` (`id`, `nama`, `isi`) VALUES
+(1, 'whatsapp', '813-9297-9131');
 
 -- --------------------------------------------------------
 
@@ -114,7 +153,9 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`id`, `nisn`, `nama`, `asal_sekolah`, `tempat_lahir`, `tanggal_lahir`, `alamat_di_yogyakarta`, `alamat_asal`, `jenis_kelamin`, `agama`, `nama_orangtua`, `alamat_orangtua`, `agama_orangtua`, `pekerjaan_orangtua`, `nama_wali`, `alamat_di_yogyakarta_orangtua`, `nama_sekolah`, `alamat_sekolah`, `nilai_bahasa_indonesia`, `nilai_bahasa_inggris`, `nilai_matematika`, `nilai_ipa`, `nilai_total_ujian`, `prestasi_non_akademis_1`, `prestasi_non_akademis_2`, `peminatan`, `diterima`) VALUES
-(1, '234234232323', 'Abu Bakar Ash Shiddiq', 'adsf', 'asdf', '2019-07-16', 'asdf', 'asdf', 'perempuan', 'buddha', 'asdf', 'asdf', 'katolik', 'adsf', 'asdf', 'asdf', 'asdf', 'adsf', 23.23, 43.23, 23.43, 43.23, 23.22, 'asdf', '', 'ipa', 0);
+(1, '234234232323', 'Abu Bakar Ash Shiddiq', 'SMP NU 3 Yogyakarta', 'asdf', '2019-07-16', 'asdf', 'asdf', 'perempuan', 'buddha', 'asdf', 'asdf', 'katolik', 'adsf', 'asdf', 'asdf', 'asdf', 'adsf', 23.23, 43.23, 23.43, 43.23, 23.22, 'asdf', '', 'ipa', 0),
+(2, '92387923792376', 'Ibnu Sina', 'SMP Muhammadiyah 1 Yogyakarta', 'asdfaf', '2019-07-01', 'adsf', 'adsf', 'laki-laki', 'buddha', 'adsf', 'adsf', 'katolik', 'adsf', 'adf', 'adsf', 'adf', 'adsf', 77.77, 82.23, 33.43, 53.23, 123.22, 'adfasdfadsf', '', 'ipa', 0),
+(8, '23423423423', 'Aisyah RA', 'SMP Al Azhar 3 Malang', 'asdfasdfasdf', '2019-07-08', 'asdf', 'asdf', 'perempuan', 'kristen', 'asdfasd', 'asdf', 'islam', 'asdfasdf', 'asdf', 'asdf', 'asdf', 'asdf', 53.23, 90.2, 23.5, 12.23, 101.52, 'Aafdsfasdf', 'Tasdfasdf', 'ipa', 0);
 
 -- --------------------------------------------------------
 
@@ -149,8 +190,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `email`, `activation_selector`, `activation_code`, `forgotten_password_selector`, `forgotten_password_code`, `forgotten_password_time`, `remember_selector`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
-(1, '127.0.0.1', 'administrator', '$2y$12$kIglgZJkh.2spNFsbV1yFeC.snoKsIbpsRUNSSyQ.FOV.YjsokA9G', 'admin@admin.com', NULL, '', NULL, NULL, NULL, NULL, NULL, 1268889823, 1563462482, 1, 'Admin', 'istrator', 'ADMIN', '0'),
-(2, '::1', 'timtam@ppdb.innaweb.com', '$2y$12$uqTkQMMFWOiuTRCoCeC1ReiXR8yqSkR9QPnbAS96yvE6Na1h9XVEC', 'timtam@ppdb.innaweb.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1562571957, 1562858098, 1, 'Tim', 'Tam', 'SMA Muhammadiyah 3', '');
+(1, '127.0.0.1', 'administrator', '$2y$12$kIglgZJkh.2spNFsbV1yFeC.snoKsIbpsRUNSSyQ.FOV.YjsokA9G', 'admin@admin.com', NULL, '', NULL, NULL, NULL, NULL, NULL, 1268889823, 1562765225, 1, 'Admin', 'istrator', 'ADMIN', '0'),
+(2, '::1', 'timtam@ppdb.innaweb.com', '$2y$12$uqTkQMMFWOiuTRCoCeC1ReiXR8yqSkR9QPnbAS96yvE6Na1h9XVEC', 'timtam@ppdb.innaweb.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1562571957, 1563470288, 1, 'Tim', 'Tam', 'SMA Muhammadiyah 3', '');
 
 -- --------------------------------------------------------
 
@@ -179,6 +220,12 @@ INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
 --
 
 --
+-- Indexes for table `berita`
+--
+ALTER TABLE `berita`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `groups`
 --
 ALTER TABLE `groups`
@@ -188,6 +235,12 @@ ALTER TABLE `groups`
 -- Indexes for table `login_attempts`
 --
 ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengaturan`
+--
+ALTER TABLE `pengaturan`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -227,6 +280,12 @@ ALTER TABLE `users_groups`
 --
 
 --
+-- AUTO_INCREMENT for table `berita`
+--
+ALTER TABLE `berita`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
@@ -239,10 +298,16 @@ ALTER TABLE `login_attempts`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `pengaturan`
+--
+ALTER TABLE `pengaturan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
