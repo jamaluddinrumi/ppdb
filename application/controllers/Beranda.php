345e8ed8f5f42d0a663b6013b568263fc70103df
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-08-09 21:54:52 +0700

    remove all comments

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -3,24 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Beranda extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-
 		$this->load->view('header');
 
 		$this->load->view('banner');
@@ -29,13 +13,9 @@ class Beranda extends CI_Controller {
 		$data['whatsapp'] = $this->Pengaturan_model->get_whatsapp();
 
 		$this->load->model('Persyaratan_model');
-		$persyaratan = $this->Persyaratan_model->get_persyaratan();
-
-		$data['persyaratan'] = $persyaratan;
+		$data['persyaratan'] = $this->Persyaratan_model->get_persyaratan();
 
 		$this->load->view('persyaratan', $data);
-
-		// $this->load->view('daftar_berita');
 
 		$this->load->view('footer', $data);
 	}
