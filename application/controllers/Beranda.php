9e86195e513ec4dcab1181bc1ffbe7c74fa2c9a5
Gangsar Swapurba <gangsar.swapurba@gmail.com>
2019-08-09 21:58:36 +0700

    make it like a poetry

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -5,15 +5,15 @@ class Beranda extends CI_Controller {
 
 	public function index()
 	{
+			$this->load->model('Pengaturan_model');
+			$data['whatsapp'] = $this->Pengaturan_model->get_whatsapp();
+
+			$this->load->model('Persyaratan_model');
+			$data['persyaratan'] = $this->Persyaratan_model->get_persyaratan();
+
 		$this->load->view('header');
 
 		$this->load->view('banner');
-
-		$this->load->model('Pengaturan_model');
-		$data['whatsapp'] = $this->Pengaturan_model->get_whatsapp();
-
-		$this->load->model('Persyaratan_model');
-		$data['persyaratan'] = $this->Persyaratan_model->get_persyaratan();
 
 		$this->load->view('persyaratan', $data);
 
